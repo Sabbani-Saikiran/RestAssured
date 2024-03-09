5a384f56d810844307c8846c5008980f40f105af
saikiran <saikiran@gmail.com>
2024-03-09 07:28:45 +0530

    first commit

---
+++ src/test/java/multiple_environments/GETDemo.java
@@ -0,0 +1,22 @@
+package multiple_environments;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+
+public class GETDemo {
+
+	@Test
+	public void getServerResponse() {
+	RestAssured
+	 .when()
+	 .get("https://restcountries.com/v3.1/all")
+	 .then()
+	 .assertThat()
+	 .statusCode(200)
+	 .log().all();
+	}
+	}
+	
+
+
